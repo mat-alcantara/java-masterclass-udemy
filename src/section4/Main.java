1686a34404af3c4da61e21bddd8fd540db6d8923
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-10-23 10:39:30 -0300

    add EqualSumChecker

--- src/section4/Main.java
+++ src/section4/Main.java
@@ -11,10 +11,13 @@ public class Main {
         // Barking Dog
         BarkingDog.shouldWakeUp(true, 8);
 
-        // LeapYearCalculator
+        // Leap Year Calculator
         LeapYearCalculator.isLeapYear(1990);
 
-        // DecimalComparator
+        // Decimal Comparator
         DecimalComparator.areEqualByThreeDecimalPlaces(1.754, 3.678);
+
+        // Equal Sum Checker
+        EqualSumChecker.hasEqualSum(2, 3, 5);
     }
 }
