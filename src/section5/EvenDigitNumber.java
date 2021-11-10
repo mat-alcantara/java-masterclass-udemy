ece9ef4baa55ff924809a1f9a84be3e3e0b72b53
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-11-10 01:35:41 -0300

    Add EvenDigitNumber

---
+++ src/section5/EvenDigitNumber.java
@@ -0,0 +1,25 @@
+public class EvenDigitSum {
+    public static int getEvenDigitSum(int number) {
+        if (number < 0) {
+            return -1;
+        }
+
+        int result = 0;
+        int tmp = 0;
+
+        // Sum all even numbers between 0 and number
+        while(number > 0) {
+            tmp = number % 10;
+
+            if(tmp % 2 == 0) {
+                result += tmp;
+            }
+
+            number /= 10;
+
+
+        }
+
+        return result;
+    }
+}
