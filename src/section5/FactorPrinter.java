1218694886ec99c4e52baed9210a0e3533ddc9c5
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-11-28 16:40:56 -0300

    add FactorPrinter

---
+++ src/section5/FactorPrinter.java
@@ -0,0 +1,19 @@
+package section5;
+
+public class FactorPrinter {
+    public static void printFactors(int number) {
+        if (number < 1) {
+            System.out.println("Invalid Value");
+        }
+
+        int counter = 1;
+
+        while (counter <= number) {
+            if (number % counter == 0) {
+                System.out.println(counter);
+            }
+
+            counter += 1;
+        }
+    }
+}
