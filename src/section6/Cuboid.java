3faa3691cdf575fac0644d7f713b3e9f31729573
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-12-14 07:40:11 -0300

    add Cuboid

---
+++ src/section6/Cuboid.java
@@ -0,0 +1,18 @@
+package section6;
+
+public class Cuboid extends Rectangle {
+    private final double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        this.height = height < 0 ? 0 : height;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public double getVolume() {
+        return super.getArea() * this.getHeight();
+    }
+}
