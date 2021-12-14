d6fc1ee9601600f64f1ba26075698544733b668a
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-12-14 07:36:58 -0300

    add Rectangle

---
+++ src/section6/Rectangle.java
@@ -0,0 +1,23 @@
+package section6;
+
+public class Rectangle {
+    private final double width;
+    private final double length;
+
+    public Rectangle(double width, double length) {
+        this.width = width < 0 ? 0 : width;
+        this.length = length < 0 ? 0 : length;
+    }
+
+    public double getWidth() {
+        return this.width;
+    }
+
+    public double getLength() {
+        return this.length;
+    }
+
+    public double getArea() {
+        return this.getWidth() * this.getLength();
+    }
+}
